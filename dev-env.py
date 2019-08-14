57d1e54603e553487725240e27e753663d98eda7
anouarbensaad <bensaad.tig@gmail.com>
2019-08-14 15:16:41 +0100

    fix whiptail

--- dev-env.py
+++ dev-env.py
@@ -1,10 +1,7 @@
 
 from __future__ import print_function
 import os, sys , time ,subprocess
-print ('''
-========= MENU ==========
-1- Create VM
-''')
+
 def user():
     U = os.popen("who |tr -s ' ' |cut -d' ' -f1").read().splitlines()
     for user in U:
@@ -17,67 +14,43 @@ def gid():
     G = os.popen("cat /etc/passwd|grep %s|cut -d ':' -f4" % user()).read().splitlines()
     for gid in G:
         return int(gid)
-def vagrantconf():
-        path= ('/home/%s/.vagrant.d/' % user())
-        os.chown(path,uid(),gid())
-        print('permission changed.')
 
-def envtest():
+def Menu():
     # Choose Envirement Type.
-    ENV_TYPE = ['whiptail', '--title', "Envirement Type", '--menu', "Choose Envirement", '11', '73', '3',
+    ENV_TYPE = ['whiptail', '--title',  "Envirement Type", '--menu', "Choose Envirement", '11', '73', '3',
     "Front End", " Allow connections to other hosts",
     "Back End", " Allow connections from other hosts",
-    "Other ", " Allow mounting of local devices",]
-    p = subprocess.Popen(ENV_TYPE)
-    p.communicate()
-
-    # Front End Envirement.
-    FE_ENV = ['whiptail', '--title', "Front End Envirments", '--checklist', "Choose Frameworks", '20', '78', '4',
-    " Angular", " Allow connections to other hosts", "ON",
-    " React", " Allow connections from other hosts", "OFF",
-    " Vue.JS", " Allow mounting of local devices", "OFF",
-    " Preact.JS", " Allow mounting of remote devices", "OFF"]
-    p = subprocess.Popen(FE_ENV)
-    p.communicate()
-
+    "Other", " Allow mounting of local devices",
+    ]
+    envtype = subprocess.run(ENV_TYPE , stderr=subprocess.PIPE).stderr.decode('utf-8')
+    print(envtype)
+    if envtype == "Front End" : 
+        FE_ENV = ['whiptail', '--title', "Front End Envirments", '--checklist', "Choose Frameworks", '20', '78', '4',
+        "Angular", " Allow connections to other hosts", "OFF",
+        "React", " Allow connections from other hosts", "OFF",
+        "Vue.JS", " Allow mounting of local devices", "OFF",
+        "Preact.JS", " Allow mounting of remote devices", "OFF"]
+        feenv = subprocess.run(FE_ENV , stderr=subprocess.PIPE).stderr.decode('utf-8')
+        print(feenv)
+    elif envtype == "Back End":
+        BE_ENV = ['whiptail', '--title', "Back End Envirments", '--checklist', "Choose Frameworks", '20', '78', '6',
+        "Spring Boot", " Allow connections to other hosts", "OFF",
+        "Express", " Allow connections from other hosts", "OFF",
+        "Ruby on Rails", " Allow mounting of local devices", "OFF",
+        "Laravel Lumen ", " Allow mounting of remote devices", "OFF",
+        "Django", " Allow connections from other hosts", "OFF",
+        "Symfony", " Allow mounting of local devices", "OFF",]
+        beenv = subprocess.run(BE_ENV , stderr=subprocess.PIPE).stderr.decode('utf-8')
+        print(beenv)
+    #p.communicate()
     # Back End Envirement.
-    BE_ENV = ['whiptail', '--title', "Back End Envirments", '--checklist', "Choose Frameworks", '20', '78', '4',
-    " Spring Boot", " Allow connections to other hosts", "ON",
-    " Express", " Allow connections from other hosts", "OFF",
-    " Ruby on Rails", " Allow mounting of local devices", "OFF",
-    " Laravel Lumen ", " Allow mounting of remote devices", "OFF",
-    " Django", " Allow connections from other hosts", "OFF",
-    " Symfony", " Allow mounting of local devices", "OFF",]
-    p = subprocess.Popen(BE_ENV)
-    p.communicate()
-
-    # Others Envirement.
-    O_ENV = ['whiptail', '--title', "Other Envirments", '--checklist', "Choose Apps", '20', '78', '4',
-    " Apache2", " Allow connections to other hosts", "ON",
-    " Nginx", " Allow connections from other hosts", "OFF",
-    " php7.2", " Allow mounting of local devices", "OFF",]
-    p = subprocess.Popen(O_ENV)
-    p.communicate()
-
-# Generate Vagrantfile
-
-def vagrantgen():
-    NAME     = input("VM Name : ")
-    MEM      = input("VM Memory : ")
-    envtest()
-    f= open("Vagrantfile","w+")
-    f.write('''
-Vagrant.configure("2") do |config|
- config.vm.box = "ubuntu/trusty64"
- config.vm.provider "virtualbox" do |vb|
-  vb.gui = true
-  vb.name   = "%s"
-  vb.memory = "%s"
- end
-end
-    ''' % (NAME , MEM))
-    f.close()
-
-
-vagrantconf()
-vagrantgen()
+    else:
+        # Others Envirement.
+        O_ENV = ['whiptail', '--title', "Other Envirments", '--checklist', "Choose Apps", '20', '78', '3',
+        "Apache2", " Allow connections to other hosts", "OFF",
+        "Nginx", " Allow connections from other hosts", "OFF",
+        "php7.2", " Allow mounting of local devices", "OFF",]
+        oenv = subprocess.run(O_ENV , stderr=subprocess.PIPE).stderr.decode('utf-8')
+        print(oenv)
+# Generate AnsibleContainer
+Menu()
